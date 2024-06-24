1139f092e7753001668f304906899ebecdf7f0ec
ARMAS <nugiarmasnurizky@gmail.com>
2024-06-24 19:21:51 +0700

    tipedata

---
+++ src/string.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var firstName : String = "AJI"
+    var LastName = "nugiarmas"
+
+    var fullName : String = "SfirstName SLastName"
+
+
+    println(firstName)
+    println(LastName)
+    println(fullName)
+}
